bd6f5daf9ba2316c7130d8f86803b89721f65b43
Linus <linuslee2000@gmail.com>
2023-07-28 15:31:35 +0800

    fix: make clipboard only work when in wsl environment
    I'm not sure that this works in WSL yet, but it works on Linux machine.

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -3,16 +3,18 @@
 -- Add any additional options here
 
 vim.cmd([[
-let g:clipboard = {
-\   'name': 'WslClipboard',
-\   'copy': {
-\      '+': 'clip.exe',
-\      '*': 'clip.exe',
-\    },
-\   'paste': {
-\      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-\      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-\   },
-\   'cache_enabled': 0,
-\ }
+if has ("wsl")
+  let g:clipboard = {
+  \   'name': 'WslClipboard',
+  \   'copy': {
+  \      '+': 'clip.exe',
+  \      '*': 'clip.exe',
+  \    },
+  \   'paste': {
+  \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  \   },
+  \   'cache_enabled': 0,
+  \ }
+endif
 ]])
